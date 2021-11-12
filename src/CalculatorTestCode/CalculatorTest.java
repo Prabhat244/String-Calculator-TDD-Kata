d3d9b99df6854c487d94058c5bd10527eb17ac39
Prabhat244 <prabhatsingh4873@gmail.com>
2021-11-13 01:28:50 +0530

    calculator working with one number fixed

--- src/CalculatorTestCode/CalculatorTest.java
+++ src/CalculatorTestCode/CalculatorTest.java
@@ -6,15 +6,14 @@ import org.junit.Test;
 import CalculatorMainCode.Calculator;
 
 public class CalculatorTest {
-
 	public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("CalculatorTestCode");
     }
-
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
+
 	@Test
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
